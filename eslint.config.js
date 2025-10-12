4941ed036464b31cbdf4018d7861804ce72c5ac5
Lucio Neves <luciocecilia20@gmail.com>
2025-10-12 17:23:51 -0300

    add prettier tailwind plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import eslintConfigPrettier from 'eslint-config-prettier'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
 
 export default [
   { ignores: ['dist'] },
