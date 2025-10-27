f8957cd5f7cc47cf858b75d8a3af3c34692dc87a
Lucio Neves <luciocecilia20@gmail.com>
2025-10-27 20:18:37 -0300

    feat: add api integration on signup page

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-export const api = axios.create({
-  baseURL: 'https://fullstackclub-finance-dashboard-api.onrender.com',
+export const publicApi = axios.create({
+  baseURL: 'https://fullstackclub-finance-dashboard-api.onrender.com/api',
 })
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,9 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
+import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import PasswordInput from '@/components/password-input'
@@ -23,6 +26,7 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { publicApi } from '@/lib/axios'
 
 const signupSchema = z.object({
   firstName: z.string().trim().min(1, { message: 'O nome é obrigatório' }),
@@ -46,6 +50,19 @@ const signupSchema = z.object({
 })
 
 const SignupPage = () => {
+  const [user, setUser] = useState(null)
+  const SignupMutation = useMutation({
+    mutationKey: ['signup'],
+    mutationFn: async (variables) => {
+      const response = await publicApi.post('/users', {
+        first_name: variables.firstName,
+        last_name: variables.lastName,
+        email: variables.email,
+        password: variables.password,
+      })
+      return response.data
+    },
+  })
   const form = useForm({
     resolver: zodResolver(signupSchema),
     defaultValues: {
@@ -59,7 +76,23 @@ const SignupPage = () => {
   })
 
   const handleSubmit = (data) => {
-    console.log(data)
+    SignupMutation.mutate(data, {
+      onSuccess: (createdUser) => {
+        const accessToken = createdUser.tokens.accessToken
+        const refresshToken = createdUser.tokens.refreshToken
+        setUser(createdUser)
+        localStorage.setItem('accessToken', accessToken)
+        localStorage.setItem('refreshToken', refresshToken)
+        toast.success('Conta criada com sucesso!')
+      },
+      onError: () => {
+        toast.error('Erro ao criar a conta. Tente novamente.')
+      },
+    })
+  }
+
+  if (user) {
+    return <h1>Usuário criado com sucesso!</h1>
   }
 
   return (
