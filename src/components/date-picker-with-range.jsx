2af6670f5bbebf6ea2ab8a9ea8725a5978b6b956
Lucio Neves <luciocecilia20@gmail.com>
2025-11-04 15:56:25 -0300

    feat: ensure calendar language is ptbr

--- src/components/date-picker-with-range.jsx
+++ src/components/date-picker-with-range.jsx
@@ -60,6 +60,7 @@ export const DatePickerWithRange = ({
             onSelect={onChange}
             initialFocus
             numberOfMonths={2}
+            locale={ptBR}
           />
         </PopoverContent>
       </Popover>
