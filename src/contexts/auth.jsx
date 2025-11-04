37464ba011643e3a6dfba57dc5082afbe993334f
Lucio Neves <luciocecilia20@gmail.com>
2025-11-04 08:08:03 -0300

    refactor: add user service to signup

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -7,6 +7,7 @@ import {
   LOCAL_STORAGE_REFRESH_TOKEN_KEY,
 } from '@/constants/local-storage'
 import { protectedApi, publicApi } from '@/lib/axios'
+import { UserService } from '@/services/user'
 
 export const AuthContext = createContext({
   user: null,
@@ -34,13 +35,8 @@ export const AuthContextProvider = ({ children }) => {
   const SignupMutation = useMutation({
     mutationKey: ['signup'],
     mutationFn: async (variables) => {
-      const response = await publicApi.post('/users', {
-        first_name: variables.firstName,
-        last_name: variables.lastName,
-        email: variables.email,
-        password: variables.password,
-      })
-      return response.data
+      const response = await UserService.signup(variables)
+      return response
     },
   })
 
