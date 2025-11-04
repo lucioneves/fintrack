a6dfe47e807a9a7774dc9680ef9e42d024329a81
Lucio Neves <luciocecilia20@gmail.com>
2025-11-04 16:44:14 -0300

    feat: ensure selected date is persistend on page reload

--- src/components/date-seletction.jsx
+++ src/components/date-seletction.jsx
@@ -1,13 +1,31 @@
-import { addMonths } from 'date-fns'
-import { useState } from 'react'
+import { addMonths, format } from 'date-fns'
+import { useEffect, useState } from 'react'
+import { useNavigate, useSearchParams } from 'react-router'
 
 import { DatePickerWithRange } from './date-picker-with-range'
 
+const formatDateToQueryParam = (date) => format(date, 'yyyy-MM-dd')
+
 const DateSelection = () => {
+  const [searchParams] = useSearchParams()
+  const navigate = useNavigate()
   const [date, setDate] = useState({
-    from: new Date(),
-    to: addMonths(new Date(), 1),
+    from: searchParams.get('from')
+      ? new Date(searchParams.get('from') + 'T00:00:00')
+      : new Date(),
+    to: searchParams.get('to')
+      ? new Date(searchParams.get('to') + 'T00:00:00')
+      : addMonths(new Date(), 1),
   })
+
+  useEffect(() => {
+    if (!date?.from || !date?.to) return
+    const queryParams = new URLSearchParams()
+    queryParams.set('from', formatDateToQueryParam(date.from))
+    queryParams.set('to', formatDateToQueryParam(date.to))
+    navigate(`/?${queryParams.toString()}`)
+  }, [navigate, date])
+
   return <DatePickerWithRange value={date} onChange={setDate} />
 }
 
