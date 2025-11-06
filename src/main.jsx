08337276c267b3b1c458d9c4256c88d67e119a86
Lucio Neves <luciocecilia20@gmail.com>
2025-11-06 17:25:47 -0300

    feat: add Stale time and ensure balance query is only when from and to are provided

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import './index.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
@@ -17,6 +18,7 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <AuthContextProvider>
         <BrowserRouter>
           <Routes>
