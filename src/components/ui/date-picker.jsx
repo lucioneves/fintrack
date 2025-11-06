38a891aedfdc2759d085e49da9f6a4e48d94a952
Lucio Neves <luciocecilia20@gmail.com>
2025-11-06 12:43:38 -0300

    feat: add date picker and dialog components

---
+++ src/components/ui/date-picker.jsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale/pt-BR'
+import { CalendarIcon } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+
+export const DatePicker = ({ value, onChange, placeholder }) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          id="date"
+          className={cn(
+            'w-full justify-start text-left font-normal',
+            !value && 'text-muted-foreground'
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {value ? (
+            format(value, 'PPP', { locale: ptBR })
+          ) : (
+            <span>{placeholder}</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+        <Calendar
+          mode="single"
+          selected={value}
+          onSelect={onChange}
+          initialFocus
+          locale={ptBR}
+        />
+      </PopoverContent>
+    </Popover>
+  )
+}
