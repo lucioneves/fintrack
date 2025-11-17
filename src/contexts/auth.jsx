42fa0f58c4c581058560e49ea2bde4960443fbe7
Lucio Neves <luciocecilia20@gmail.com>
2025-11-17 17:57:13 -0300

    feat: add loading to login button and add login user form hook

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -69,17 +69,15 @@ export const AuthContextProvider = ({ children }) => {
     })
   }
 
-  const login = (data) => {
-    loginMutation.mutate(data, {
-      onSuccess: (loggedUser) => {
-        setUser(loggedUser)
-        setTokens(loggedUser.tokens)
-        toast.success('Login realizado com sucesso!')
-      },
-      onError: (error) => {
-        console.error(error)
-      },
-    })
+  const login = async (data) => {
+    try {
+      const loggedUser = await loginMutation.mutateAsync(data)
+      setUser(loggedUser)
+      setTokens(loggedUser.tokens)
+      toast.success('Login realizado com sucesso!')
+    } catch (error) {
+      console.error(error)
+    }
   }
 
   const signOut = () => {
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,5 @@
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+import { Loader2Icon } from 'lucide-react'
 import { Link, Navigate } from 'react-router'
-import z from 'zod'
 
 import PasswordInput from '@/components/password-input'
 import { Button } from '@/components/ui/button'
@@ -23,28 +21,11 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { useAuthContext } from '@/contexts/auth'
-
-const loginShema = z.object({
-  email: z
-    .string()
-    .email({ message: 'O e-mail é inválido' })
-    .trim()
-    .min(1, { message: 'O e-mail é obrigatório' }),
-  password: z
-    .string()
-    .trim()
-    .min(6, { message: 'A senha deve ter pelo menos 6 caracteres' }),
-})
+import { useLoginForm } from '@/forms/hooks/user'
 
 const LoginPage = () => {
   const { user, login, isInitializing } = useAuthContext()
-  const form = useForm({
-    resolver: zodResolver(loginShema),
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-  })
+  const { form } = useLoginForm()
 
   const handleSubmit = (data) => login(data)
 
@@ -95,7 +76,12 @@ const LoginPage = () => {
               />
             </CardContent>
             <CardFooter>
-              <Button className="w-full">Fazer login</Button>
+              <Button className="w-full" disabled={form.formState.isSubmitting}>
+                {form.formState.isSubmitting && (
+                  <Loader2Icon className="animate-spin" />
+                )}
+                Fazer login
+              </Button>
             </CardFooter>
           </Card>
         </form>
