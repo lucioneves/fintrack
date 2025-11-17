3f8aa92514015b711c8f0b9e5f7cc4da27d7dae4
Lucio Neves <luciocecilia20@gmail.com>
2025-11-17 16:56:52 -0300

    fix: ensure user balance query is properly invalidated

--- src/api/hooks/user.js
+++ src/api/hooks/user.js
@@ -4,6 +4,9 @@ import { UserService } from '@/api/services/user'
 import { useAuthContext } from '@/contexts/auth'
 
 export const getUserBalanceQueryKey = ({ userId, from, to }) => {
+  if (!from || !to) {
+    return ['balance', userId]
+  }
   return ['balance', userId, from, to]
 }
 export const useGetUserBalance = ({ from, to }) => {
